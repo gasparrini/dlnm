c7bbcbbf794a2c1de3e509344e9590e6ac56ccc8
Antonio Gasparrini <gaspanero@hotmail.com>
2016-12-30 23:40:39 +0100

    Minor change in automatic selection of centering value

--- R/mkcen.R
+++ R/mkcen.R
@@ -1,31 +1,48 @@
 ###
-### R routines for the R package dlnm (c) Antonio Gasparrini 2016
+### R routines for the R package dlnm (c) Antonio Gasparrini 2016-2017
 #
-mkcen <-
-function(cen, type, basis, range) {
+mkcen <- function(cen, type, basis, range) {
 #
 ################################################################################
 #
   # IF NULL, TRY TO EXTRACT IT FROM BASIS
-  if(is.null(cen)) cen <- switch(type,cb=attributes(basis)$argvar$cen,
-    one=attributes(basis)$cen,gam=NULL)
-  #
+  if (nocen <- is.null(cen))
+    cen <- switch(
+      type,
+      cb = attributes(basis)$argvar$cen,
+      one = attributes(basis)$cen,
+      gam = NULL
+    )
+#
   # DEPENDING ON FUNCTION
-  fun <- switch(type,cb=attributes(basis)$argvar$fun,
-    one=attributes(basis)$fun,gam=basis$margin[[1]]$fun)
-  if(!is.null(fun) && fun%in%c("thr","strata","integer","lin")) {
-    if(is.logical(cen)) cen <- NULL
+  fun <- switch(
+    type,
+    cb = attributes(basis)$argvar$fun,
+    one = attributes(basis)$fun,
+    gam = basis$margin[[1]]$fun
+  )
+  # SET CENTERING DEPENDING ON FUNCTION AND cen TYPE
+  if (!is.null(fun) && fun %in% c("thr", "strata", "integer", "lin")) {
+    if (is.logical(cen)) cen <- NULL
   } else {
-    # IF NULL OR TRUE, MID-RANGE
-    if(is.null(cen) || is.logical(cen)&&cen) cen <- mean(range)
+    # IF NULL OR TRUE, SET TO (APPROXIMATELY) MID-RANGE AND MESSAGE
+    if (is.null(cen) || (is.logical(cen) && cen)) cen <- median(pretty(range))
     # IF FALSE, NULL
-    if(is.logical(cen)&&!cen) cen <- NULL
+    if (is.logical(cen) && !cen) cen <- NULL
   }
 #
   # HOWEVER, IF INTERCEPT IS PRESENT, SET TO NULL
-  int <- switch(type,cb=attributes(basis)$argvar$intercept,
-    one=attributes(basis)$intercept,gam=basis$margin[[1]]$intercept)
-  if(is.logical(int)&&int) cen <- NULL
-  #
+  int <- switch(
+    type,
+    cb = attributes(basis)$argvar$intercept,
+    one = attributes(basis)$intercept,
+    gam = basis$margin[[1]]$intercept
+  )
+  if (is.logical(int) && int) cen <- NULL
+#
+  # MESSAGE
+  if(nocen && !is.null(cen))
+    message("centering value unspecified. Automatically set to mid-range of predictor")
+#
   return(cen)
 }
