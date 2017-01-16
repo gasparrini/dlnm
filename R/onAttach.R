486412166fe1e6f9e2f77a4e1127ceeabfd8cf5e
Antonio Gasparrini <gaspanero@hotmail.com>
2017-01-16 01:29:49 +0000

    Change onAttach message

--- R/onAttach.R
+++ R/onAttach.R
@@ -9,8 +9,8 @@ function(lib, pkg) {
   meta <- packageDescription("dlnm")
   attachmsg <- paste("This is dlnm ",meta$Version,
     ". For details: help(dlnm) and vignette('dlnmOverview').",
-    "\n",
-    "Important changes: see file.show(system.file('Changesince220',package='dlnm'))",
+    #"\n",
+    #"Important changes: see file.show(system.file('Changesince220',package='dlnm'))",
     sep="")
   packageStartupMessage(attachmsg, domain = NULL, appendLF = TRUE)
 }
