bd80739126d9c75215ad1a6fde87f24837bed5e2
Aariq <scottericr@gmail.com>
2020-10-19 17:31:19 -0400

    added a test

--- tests/testthat/test-smooth.construct.cb.smooth.R
+++ tests/testthat/test-smooth.construct.cb.smooth.R
@@ -2,27 +2,29 @@ library(mgcv)
 data(nested)
 Q <- as.matrix(nested[, 5:14]) 
 L <- matrix(1:ncol(Q), nrow = nrow(Q), ncol = ncol(Q), byrow = TRUE)
+xt1 <- list(bs = c("cr", "cr"))
+xt2 <- list(bs = c("cr", "cs"))
+m_cr <- gam(case ~ age + s(Q, L, bs = "cb", xt = xt1), family = binomial, data = nested)
+m_cs <- gam(case ~ age + s(Q, L, bs = "cb", xt = xt2), family = binomial, data = nested)
+
 
 test_that("penalty matrix is formed correctly for bs = 'cs'", {
-  xt <-
-    list(
-      bs = c("cr", "cs"),
-      arglag = list(int = F)
-    )
-  m_cs <- gam(case ~ age + s(Q, L, bs = "cb", xt = xt), family = binomial, data = nested)
-  
   expect_length(m_cs$sp, 2)
   expect_named(m_cs$sp, c("s(Q,L)Svar", "s(Q,L)Slag"))
 })
 
 test_that("penalty matrix is formed correctly for bs = 'cr'", {
-  xt <-
-    list(
-      bs = c("cr", "cr"),
-      arglag = list(int = F)
-    )
-  m_cs <- gam(case ~ age + s(Q, L, bs = "cb", xt = xt), family = binomial, data = nested)
-  
-  expect_length(m_cs$sp, 2)
-  expect_named(m_cs$sp, c("s(Q,L)Svar", "s(Q,L)Slag"))
+  expect_length(m_cr$sp, 2)
+  expect_named(m_cr$sp, c("s(Q,L)Svar", "s(Q,L)Slag"))
+})
+
+test_that("penalty matrixes differ", {
+  expect_false(all.equal(m_cs$sp, m_cr$sp, check.attributes = FALSE))
+})
+
+test_that("defining marginal basis equivalant between bs = and arglag/argvar", {
+  xt3 <- list(argvar = list(fun = "cr"),
+              arglag = list(fun = "cr"))
+  m_cr2 <- gam(case ~ age + s(Q, L, bs = "cb", xt = xt3), family = binomial, data = nested)
+  expect_equivalent(m_cr, m_cr2)
 })
